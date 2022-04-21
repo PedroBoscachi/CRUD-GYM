b3a9a51418a89441b666e51eed8fc3b204b08313
PedroBoscachi <pedroboscachi@gmail.com>
2022-04-21 15:46:11 -0300

    view employees started

---
+++ Gym/Gym/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Gym.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gym.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Employees()
+        {
+            return View(Employee.Employees);
+        }
+    }
+}
---
+++ Gym/Gym/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gym.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        private static List<Employee> employees = new List<Employee>();
+        public static IQueryable<Employee> Employees
+        {
+            get
+            {
+                return employees.AsQueryable();
+            }
+        }
+
+        static Employee()
+        {
+            Employee.employees.Add(
+                new Employee { Id = 1, Name = "Raphael", Email = "raphael@gmail.com" });
+            Employee.employees.Add(
+                new Employee { Id = 2, Name = "Bruce", Email = "bruce@gmail.com" });
+            Employee.employees.Add(
+                new Employee { Id = 3, Name = "James", Email = "james@gmail.com" });
+            Employee.employees.Add(
+                new Employee { Id = 4, Name = "Selina", Email = "selina@gmail.com" });
+            Employee.employees.Add(
+                new Employee { Id = 5, Name = "Vick", Email = "vick@gmail.com" });
+        }
+    }
+}
